5142cad797d9617ec0b1744040526d8ac578aa26
junsai <junsai0530@gmail.com>
2022-04-20 22:26:19 +0900

    profile view

--- Twitter/Core/Profile/Views/ProfileView.swift
+++ Twitter/Core/Profile/Views/ProfileView.swift
@@ -1,3 +1,178 @@
 
+//
+//  Profile.swift
+//  Twitter
+//
+//  Created by 小暮準才 on 2022/04/17.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    @State private var selectionFilter: TweetFilterViewModel = .tweets
+    @Environment(\.presentationMode) var mode
+    @Namespace var animation
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            headerView
+            
+            actionButtons
+            
+            userInfoDetails
+            
+            tweetFilterBar
+            
+            tweetView
+            
+
+            Spacer()
+        }
+    }
+}
+
+struct Profile_Previews2: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
 
 
+extension ProfileView {
+    var headerView: some View {
+        ZStack (alignment: .bottomLeading) {
+            Color(.systemBlue)
+                .ignoresSafeArea()
+            
+            VStack {
+                Button {
+                    mode.wrappedValue.dismiss()
+                    
+                } label : {
+                    Image(systemName: "arrow.left")
+                        .resizable()
+                        .frame(width: 20, height: 16)
+                        .foregroundColor(.white)
+                        .offset(x: 16, y: 12)
+                }
+                
+                Circle()
+                    .frame(width: 72, height: 72)
+                .offset(x: 16, y:24)
+            }
+        }
+        .frame(height: 96)
+    }
+    var actionButtons: some View {
+        
+        HStack(spacing: 12){
+            Spacer()
+            
+            Image(systemName: "bell.badge")
+                .font(.title3)
+                .padding(6)
+                .overlay(Circle().stroke(Color.gray, lineWidth: 0.75))
+            
+            Button {
+                
+            } label: {
+                Text("Edit Profile")
+                    .font(.subheadline).bold()
+                    .frame(width: 120, height:32)
+                    .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.gray, lineWidth: 0.75))
+            }
+        }
+        .padding(.trailing)
+        
+    }
+    
+    var userInfoDetails: some View {
+        VStack (alignment: .leading, spacing: 4) {
+            HStack {
+                Text("Health ledad")
+                    .font(.title).bold()
+            
+                
+                Image(systemName: "checkmark.seal.fill")
+                    .foregroundColor(Color(.systemBlue))
+                
+            }
+            Text("@joker")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+            
+            Text("Your moms vaf")
+                .font(.subheadline)
+                .padding(.vertical)
+            
+            HStack(spacing: 24) {
+                HStack {
+                    Image(systemName: "mappin.and.ellipse")
+                    
+                    Text("Gothan, NY")
+                }
+                
+                HStack {
+                    Image(systemName: "link")
+                    
+                    Text("www.thejoker.com")
+                    
+                }
+            }
+            .font(.caption)
+            .foregroundColor(.gray)
+            
+            UserStateView()
+                .padding(.vertical)
+            
+        }
+        .padding(.horizontal)
+        
+        
+    }
+    
+    var tweetFilterBar: some View {
+        HStack {
+            ForEach(TweetFilterViewModel.allCases, id: \.rawValue) { item in
+                VStack {
+            Text(item.title)
+                        .font(.subheadline)
+                        .fontWeight(selectionFilter == item ? .semibold : .regular)
+                        .foregroundColor(selectionFilter == item ? .black : .gray)
+                    
+                    if selectionFilter == item {
+                        Capsule()
+                            .foregroundColor(Color(.systemBlue))
+                            .frame(height: 3)
+                            .matchedGeometryEffect(id: "filter", in: animation)
+                    } else {
+                        Capsule()
+                            .foregroundColor(Color(.clear))
+                            .frame(height: 3)
+                        
+                    }
+                }
+                .onTapGesture{
+                    withAnimation(.easeInOut) {
+                        self.selectionFilter = item
+                    }
+                }
+            }
+        }
+        .overlay(Divider().offset(x: 0, y: 16))
+        
+    }
+    
+    var tweetView: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(0 ... 9, id: \.self) { _ in
+                    TweetRowView()
+                        .padding()
+                    
+                }
+            }
+        }
+    }
+        
+}
