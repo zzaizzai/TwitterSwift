31a5168902c179c98e036bd067bafae021967c06
junsai <junsai0530@gmail.com>
2022-04-20 22:24:50 +0900

    prifile view

--- Twitter/Core/Profile/Views/ProfileView.swift
+++ Twitter/Core/Profile/Views/ProfileView.swift
@@ -1,20 +1,3 @@
-//
-//  ProfileView.swift
-//  Twitter
-//
-//  Created by 小暮準才 on 2022/04/17.
-//
 
-import SwiftUI
 
-struct ProfileView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
 
-struct ProfileView_Previews: PreviewProvider {
-    static var previews: some View {
-        ProfileView()
-    }
-}
